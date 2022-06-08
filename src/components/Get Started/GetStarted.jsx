257c3996f356ff7e393d1e33e7fb1d2d09389c3e
brianna-boughner <58961493+brianna-boughner@users.noreply.github.com>
2022-06-08 17:35:17 -0400

    Update GetStarted.jsx

--- src/components/Get Started/GetStarted.jsx
+++ src/components/Get Started/GetStarted.jsx
@@ -3,7 +3,7 @@ import './getStarted.css';
 export default function GetStarted() {
   return (
     <div style={{display: 'flex',  flexDirection:'column', justifyContent:'center', alignItems:'center', height: '80vh', paddingTop:'10%', backgroundColor:'#F6F6F6'}}>
-        <h1 style={{fontFamily: 'Quicksand, sans-serif'}}>Ready to upgrade your home?</h1>
+        <h1>Ready to upgrade your home?</h1>
         <a href="/experts" className="linkwrapper">
         <button className="getStarted">Get Started!</button>
         </a>
